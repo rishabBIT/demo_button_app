610b92169543407736c1f435758bce9391a137f4
nanodano <rishabdhar12@gmail.com>
2023-11-22 16:15:01 +0530

    button

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+            <View style={styles.container}>
+            <Button
+            onPress={() => {}}
+            title="Click"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+            />
+            </View>
+           );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+alignItems: 'center',
+justifyContent: 'center',
+},
 });
